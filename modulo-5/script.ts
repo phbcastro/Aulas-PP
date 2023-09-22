2f66cd9ed25d6c461575d7981acb9aff83bae29d
phbcastro <paulobcastro2007@gmail.com>
2023-09-22 20:42:50 -0300

    terminando modulo-5

--- modulo-5/script.ts
+++ modulo-5/script.ts
@@ -45,3 +45,55 @@ enum cores {
     BLACK = '#000',
     GRAY = '#F2F2F2'
 }
+
+// -----------interfaces----------- // 
+
+interface endereco {
+    cidade: string,
+    estado: string
+}
+
+interface aluno {
+    nome: string,
+    idade: number,
+    endereco: endereco
+}
+
+let aluno1: aluno = {
+    nome: 'Paulo',
+    idade: 24,
+    endereco: {
+        cidade: 'Belem',
+        estado: 'PA'
+    } 
+}
+
+// -----------Union & Aliases----------- // 
+
+interface ocupacao {
+    trabalho: string,
+    local?: string // o ? significa que é opcional, pode existir ou não
+}
+
+interface pessoaNova {
+    nome: string,
+    idade: number,
+    cidade: string
+}
+
+type dados = ocupacao & pessoaNova;
+
+
+const pessoa2: pessoaNova | ocupacao = {
+    nome: 'Paulo',
+    idade: 22,
+    cidade: 'Belem'
+}
+
+const pessoa3: dados = {
+    nome: 'Paulo',
+    idade: 22,
+    cidade: 'Belem',
+    trabalho: 'Programador',
+    local: 'Remoto'
+}
